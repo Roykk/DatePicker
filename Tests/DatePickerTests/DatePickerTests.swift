689910a717574568914c4612ba3038e4ec4aaccc
Roy Kjellid <roy.kjellid@gmail.com>
2020-05-29 12:28:37 +0200

    swiftpackIt

--- Tests/DatePickerTests/DatePickerTests.swift
+++ Tests/DatePickerTests/DatePickerTests.swift
@@ -6,7 +6,7 @@ final class DatePickerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DatePicker().text, "Hello, World!")
+        XCTAssertEqual("Add tests", "Add tests")
     }
 
     static var allTests = [
